a2174f37c4d1b3d8cda8d03efb821c0da14ebf89
Jaak Erisalu <jaak@thorgate.eu>
2017-08-02 14:21:25 +0300

    :)

--- field_permissions/admin.py
+++ field_permissions/admin.py
@@ -6,15 +6,12 @@ class FieldPermissionAdminMixin:
         ModelAdmin logic for removing fields when a user is found not to have change permissions.
     """
 
-    def remove_unauthorized_field(self, name):
-        del self.fields[name]
-
     def get_fields(self, request, obj=None):
         fields = super().get_fields(request, obj)
 
         for name in fields:
             if name in fields and not obj.has_field_perm(request.user, field=name):
-                self.remove_unauthorized_field(name)
+                del fields[name]
 
         return fields
 
