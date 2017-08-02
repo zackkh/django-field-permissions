faa1ae3d6cbd740554cc7c1a8a91cf872b83ed6a
Jaak Erisalu <jaak@thorgate.eu>
2017-08-02 13:56:26 +0300

    Move the User param to the form class itself for easier DX in Django Admin

--- field_permissions/forms.py
+++ field_permissions/forms.py
@@ -5,14 +5,12 @@ class FieldPermissionFormMixin:
     ModelForm logic for removing fields when a user is found not to have change permissions.
     """
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user')
-
         super(FieldPermissionFormMixin, self).__init__(*args, **kwargs)
 
         model = self.Meta.model
         model_field_names = [f.name for f in model._meta.get_fields()]  # this might be too broad
         for name in model_field_names:
-            if name in self.fields and not self.instance.has_field_perm(user, field=name):
+            if name in self.fields and not self.instance.has_field_perm(self.user, field=name):
                 self.remove_unauthorized_field(name)
 
     def remove_unauthorized_field(self, name):
