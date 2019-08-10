873928b5dba1b944de02fcf8514e731a51052926
Sleepy105 <lm.sousa@fe.up.pt>
2019-08-10 16:36:39 +0100

    Remove field from Form when 'view' or 'change' permissions are missing

--- field_permissions/forms.py
+++ field_permissions/forms.py
@@ -5,13 +5,15 @@ class FieldPermissionFormMixin:
     """
     ModelForm logic for removing fields when a user is found not to have change permissions.
     """
+
     def __init__(self, *args, **kwargs):
         super(FieldPermissionFormMixin, self).__init__(*args, **kwargs)
 
         model = self.Meta.model
-        model_field_names = [f.name for f in model._meta.get_fields()]  # this might be too broad
+        # this might be too broad
+        model_field_names = [f.name for f in model._meta.get_fields()]
         for name in model_field_names:
-            if name in self.fields and not self.instance.has_field_perm(self.user, field=name):
+            if name in self.fields and not (self.instance.has_field_perm(self.user, field=name, operation='view') and self.instance.has_field_perm(self.user, field=name, operation='change')):
                 self.remove_unauthorized_field(name)
 
     def remove_unauthorized_field(self, name):
