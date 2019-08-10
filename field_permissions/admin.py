a50fef73d15587ca9ecc5aa906de80995131bc94
Sleepy105 <lm.sousa@fe.up.pt>
2019-08-10 16:08:45 +0100

    Manage read_only fields in the admin interface

--- field_permissions/admin.py
+++ field_permissions/admin.py
@@ -11,10 +11,20 @@ class FieldPermissionAdminMixin:
 
         for name in fields:
             instance = obj or self.model()
-            if name in fields and not instance.has_field_perm(request.user, field=name):
+            if name in fields and not instance.has_field_perm(request.user, field=name, operation='view'):
                 fields.remove(name)
 
         return fields
+
+    def get_readonly_fields(self, request, obj=None):
+        readonly_fields = super().get_readonly_fields(request, obj)
+
+        for name in readonly_fields:
+            instance = obj or self.model()
+            if name in readonly_fields and not instance.has_field_perm(request.user, field=name, operation='change'):
+                readonly_fields.remove(name)
+
+        return readonly_fields
 
 
 class FieldPermissionAdmin(FieldPermissionAdminMixin, admin.ModelAdmin):
